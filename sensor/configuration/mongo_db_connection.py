40a347f2b9d81cd0b54ca2e7633105482b51579b
Bhavesh Kumar Lohar <bhaveshsisodia2@gmail.com>
2024-04-17 00:41:09 +0530

    project-setup

---
+++ sensor/configuration/mongo_db_connection.py
@@ -0,0 +1,26 @@
+import pymongo
+from sensor.constant.database import DATABASE_NAME
+import certifi
+
+
+ca = certifi.where()
+
+
+
+
+
+class MongoDBClient:
+    client= None
+    def __init__(self, database_name =DATABASE_NAME) -> None:
+        try:
+            if MongoDBClient.client is None:
+                mongo_db_url = "mongodb+srv://Bhavesh:stodybaker123@cluster0.zfyclqm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+                MongoDBClient.client = pymongo.MongoClient(mongo_db_url , tlsCAFile=ca)
+            self.client = MongoDBClient.client
+            self.database = self.client[database_name]
+            self.database_name = database_name
+
+        except Exception as e:
+            raise e
+
+

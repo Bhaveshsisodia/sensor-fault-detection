5585f8ce2531acb89b499e34cf5058bc26e1c1d3
Bhavesh Kumar Lohar <bhaveshsisodia2@gmail.com>
2024-04-21 19:32:18 +0530

    Data Transformation Done

--- sensor/components/data_validation.py
+++ sensor/components/data_validation.py
@@ -139,11 +139,14 @@ class DataValidation:
             data_validation_artifact = DataValidationArtifact(validation_status=status,
                                                               valid_train_file_path=self.data_ingestion_artifact.trained_file_path,
                                                               valid_test_file_path=self.data_ingestion_artifact.test_file_path,
-                                                              invalid_train_file_path=self.data_validation_config.invalid_train_file_path,
-                                                              invalid_test_file_path=self.data_validation_config.invalid_test_file_path,
+                                                              invalid_train_file_path=None,
+                                                              invalid_test_file_path=None,
                                                               drift_report_file_path=self.data_validation_config.drift_report_file_path
                                                               )
 
+            logging.info(f"Data Validation artifact :{data_validation_artifact}")
+
+            return data_validation_artifact
 
 
 
--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -1,10 +1,12 @@
-from sensor.entity.config_entity import TrainingPipelineConfig, DataIngestionConfig, DataValidationConfig
+from sensor.entity.config_entity import TrainingPipelineConfig, DataIngestionConfig, DataValidationConfig , DataTransformationConfig
 from sensor.logger import logging
-from sensor.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
+from sensor.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact , DataTransformationArtifact
 from sensor.exception import SensorException
 import sys, os
 from sensor.components.data_ingestion import DataIngestion
 from sensor.components.data_validation import DataValidation
+from sensor.components.data_transformation import DataTransformation
+
 class TrainPipeline:
 
     def __init__(self):
@@ -44,10 +46,17 @@ class TrainPipeline:
 
         except Exception as e:
             raise SensorException(e, sys)
-    def start_data_transformation(self):
+    def start_data_transformation(self, data_validation_artifact:DataValidationArtifact):
         try:
-            pass
+            data_transformation_config = DataTransformationConfig(training_pipeline_config=self.training_pipeline_config)
+            data_transformation=DataTransformation(data_validation_artifact=data_validation_artifact,
+                                                   data_transformation_config=data_transformation_config)
+            data_transformation_artifact = data_transformation.initiate_data_transformer()
+            logging.info(f"data transformation completed Successfully :{data_transformation_artifact}")
+            return data_transformation_artifact
+
         except Exception as e:
+
             raise SensorException(e, sys) from e
 
 
@@ -70,7 +79,11 @@ class TrainPipeline:
             raise SensorException(e, sys)
 
     def run_pipeline(self):
+        try:
 
-        data_ingestion_artifact: DataIngestionArtifact=self.start_data_ingestion()
-        data_validation_artifact=self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+            data_ingestion_artifact: DataIngestionArtifact=self.start_data_ingestion()
+            data_validation_artifact=self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+            data_transformation_artifact = self.start_data_transformation(data_validation_artifact=data_validation_artifact)
+        except Exception as e:
+            raise SensorException(e, sys)
 
